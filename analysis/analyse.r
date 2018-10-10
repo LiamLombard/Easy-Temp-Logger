be40bb87649f723e53bb69cfeb418b2de5caeff9
LiamLombard <lplombard@hotmail.co.uk>
2018-10-10 09:44:10 +0100

    Updated plot style

--- analysis/analyse.r
+++ analysis/analyse.r
@@ -13,6 +13,11 @@ data$time <- as.POSIXct(data$time,origin="1970-01-01",tz="GMT-1")
 data <- melt(data, id.vars="time")
 
 
-plot <- ggplot(data, aes(x=time, y=value, colour = variable)) + geom_line()
+plot <- ggplot(data, aes(x=time, y=value, colour = variable)) +
+geom_line() +
+xlab("Date") +
+ylab("Temperature(Celcius) / Humidty(%)") +
+theme(legend.title=element_blank()) +
+scale_colour_discrete(labels=c("Temperature", "Humidity"))
 print(plot)
 dev.off()
